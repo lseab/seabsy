e5df90a88306dee70916b8e317153adf201c74da
lse <lse@ableton.com>
2025-12-17 16:25:16 +0100

    Move grammar node definitions to dedicated module

---
+++ src/grammar.hpp
@@ -0,0 +1,39 @@
+#include <variant>
+
+#include "tokenization.hpp"
+
+
+struct NodeExprIdent {
+    Token ident;
+};
+
+
+struct NodeExprIntLit {
+    Token int_lit;
+};
+
+
+struct NodeExpr {
+    std::variant<NodeExprIdent, NodeExprIntLit> expr;
+};
+
+
+struct NodeStmtReturn {
+    NodeExpr expr;
+};
+
+
+struct NodeStmtLet {
+    Token ident;
+    NodeExpr expr;
+};
+
+
+struct NodeStmt {
+    std::variant<NodeStmtReturn, NodeStmtLet> stmt;
+};
+
+
+struct NodeProgram {
+    std::vector<NodeStmt> stmts;
+};
--- src/parsing.hpp
+++ src/parsing.hpp
@@ -1,42 +1,4 @@
-#include <variant>
-
-#include "tokenization.hpp"
-
-
-struct NodeExprIdent {
-    Token ident;
-};
-
-
-struct NodeExprIntLit {
-    Token int_lit;
-};
-
-
-struct NodeExpr {
-    std::variant<NodeExprIdent, NodeExprIntLit> expr;
-};
-
-
-struct NodeStmtReturn {
-    NodeExpr expr;
-};
-
-
-struct NodeStmtLet {
-    Token ident;
-    NodeExpr expr;
-};
-
-
-struct NodeStmt {
-    std::variant<NodeStmtReturn, NodeStmtLet> stmt;
-};
-
-
-struct NodeProgram {
-    std::vector<NodeStmt> stmts;
-};
+#include "grammar.hpp"
 
 
 class Parser {
